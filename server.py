a92d4263b8d8518b1bc5e8a3e95b95f789c4e83c
ariduki <ariduki@gmail.com>
2023-06-28 20:24:05 +0800

    add server.py

--- server.py
+++ server.py
@@ -1,5 +1,33 @@
+import os
+import grpc
+from concurrent import futures
+
 import file_transport_pb2_grpc
+import file_transport_pb2
+
+FILE_PATH = "./files"
 
 
 class FileTransportServicer(file_transport_pb2_grpc.FileServiceServicer):
-    pass
+    def Upload(self, request, context):
+        path = os.path.join(FILE_PATH, request.filename)
+        with open(path, "wb") as bin_file:
+            bin_file.write(request.body)
+        response = file_transport_pb2.UploadResponse()
+        response.result = "ok"
+        return super().Upload(request, context)
+
+
+def serve():
+    port = "19810"
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    file_transport_pb2_grpc.add_FileServiceServicer_to_server(
+        FileTransportServicer(), server)
+    server.add_insecure_port("[::]:" + port)
+    server.start()
+    print("Server started, listening on " + port)
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    serve()
