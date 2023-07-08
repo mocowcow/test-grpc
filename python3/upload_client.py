b462cd52eddfe43d3882f3e1b617d7eac4456684
ariduki <ariduki@gmail.com>
2023-07-08 20:39:33 +0800

    upd python upload client

--- python3/upload_client.py
+++ python3/upload_client.py
@@ -8,8 +8,8 @@ channel = grpc.insecure_channel('localhost:19810')
 stub = file_transport_pb2_grpc.FileServiceStub(channel)
 
 request = file_transport_pb2.UploadRequest()
-request.filename = "ffff.txt"
-request.body = b'\x21\x21\x21\x21'
+request.filename = "grpc_test.txt"
+request.body = b"hello python"
 
 response = stub.Upload(request)
 print(response)
