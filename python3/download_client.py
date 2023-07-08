e6d3fce83589045f9663dd9549dd4fe3f560448b
ariduki <ariduki@gmail.com>
2023-07-08 20:36:05 +0800

    upd python download client

--- python3/download_client.py
+++ python3/download_client.py
@@ -6,12 +6,14 @@ import file_transport_pb2
 
 FILE_PATH = "./download"
 
+if not os.path.isdir(FILE_PATH):
+    os.mkdir(FILE_PATH)
 
 channel = grpc.insecure_channel('localhost:19810')
 stub = file_transport_pb2_grpc.FileServiceStub(channel)
 
 request = file_transport_pb2.DownloadRequest()
-request.filename = "ffff.txt"
+request.filename = "grpc_test.txt"
 response = stub.Download(request)
 
 path = os.path.join(FILE_PATH, request.filename)
